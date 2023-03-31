2179e543b2eb999b00d3ae9c9b1b984910ec6b6a
Jonnvoo <Jonathanvu2065@gmail.com>
2023-03-31 03:19:34 -0700

    first componets up

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-function App (){
-    return <div> Show animal list here</div>
+import {useState} from 'react';
+function getRanAnimal(){
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random()* animals.length)]
 }
+
+function App (){
+    const [count, setCount] = useState(0);
+    const handleClick = () =>{
+       setCount (count +4 );
+    };
+    return <div>
+        <button onClick={handleClick}>Add Animal</button>
+        <div>Number of animals: {count}</div>
+    </div>
+}
+
+export default App;
