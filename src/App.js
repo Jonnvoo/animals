e501fae6ac5ba383f90d6d2166767d73efb0577e
Jonnvoo <Jonathanvu2065@gmail.com>
2023-03-31 17:42:11 -0700

    css and finalized app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import './App.css';
 import {useState} from 'react';
+import AnimaShow from './AnimalShow';
 function getRanAnimal(){
     const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
     return animals[Math.floor(Math.random()* animals.length)]
 }
 
+
 function App (){
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
+
     const handleClick = () =>{
-       setCount (count +4 );
+     setAnimals([...animals, getRanAnimal()])
     };
-    return <div>
+
+    const renderedAnimals = animals.map((animal, index)=> {
+        return <AnimaShow type={animal} key={index}/>
+    }) 
+    return(
+        <div className='app'>
         <button onClick={handleClick}>Add Animal</button>
-        <div>Number of animals: {count}</div>
+        <div className='animal-list'>{renderedAnimals}</div>
     </div>
+) 
 }
 
 export default App;
